bf1eec4fcd208a957a191dd5386d60199141af22
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-12-04 17:13:56 -0300

    feat: version with infinite loop

--- src/include/genetic.h
+++ src/include/genetic.h
@@ -10,14 +10,15 @@ using cell_type = unsigned int;
 
 class SudokuBoard {
 public:
+  SudokuBoard() {};
   SudokuBoard(std::string data_str, number_type rank);
   void print_board() const;
-  void genetic_algorithm(double mutation_rate, int max_generations);
+  SudokuBoard genetic_algorithm(double mutation_rate, int max_generations);
 
 private:
   int fitness() const;
   static std::pair<SudokuBoard, SudokuBoard> crossover(const SudokuBoard &first, const SudokuBoard &second);
-  void mutate(SudokuBoard& individual, double mutation_rate) const;
+  SudokuBoard mutate(SudokuBoard& individual, double mutation_rate) const;
   bool is_valid(number_type row, number_type col, cell_type number) const;
   SudokuBoard generate_from() const;
 
